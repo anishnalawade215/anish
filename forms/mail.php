37dad43301c82ae7fbb5758222d0c246b986be41
Anish Nalawade <135167113+anishnalawade215@users.noreply.github.com>
2023-06-04 17:50:48 +0530

    Update mail.php

--- forms/mail.php
+++ forms/mail.php
@@ -1,1 +1,9 @@
+<?php
+$to = "ayush.tinkhede1722@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "Fromanishnalawade215@gmail.com" . "\r\n" .
+"CC: anishnalawade215@gmail.com.com";
 
+mail($to,$subject,$txt,$headers);
+?>
